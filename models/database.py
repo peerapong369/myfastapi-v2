b13b31ff96f01d5b7e026cb1433385ff84e7e9db
Mr.Peerapong <peerapong.dev.369@gmail.com>
2022-08-24 23:36:42 +0700

    change engin

--- models/database.py
+++ models/database.py
@@ -20,6 +20,7 @@ else:
     DATABASE_NAME = config("DATABASE_NAME")
     DATABASE_HOST = config("DATABASE_HOST")
     SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://(DATABASE_USER):(DATABASE_PASSWORD)@(DATABASE_HOST)/(DATABASE_NAME)'
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
